b37560d7950624bf05a00718d8646ccf37d18ad2
zchakraborty <zinichakraborty@gmail.com>
2023-02-07 19:21:32 -0500

    update exer4 with async/await

--- exer4/index.js
+++ exer4/index.js
@@ -1,19 +1,29 @@
 import figlet from "figlet";
-const { textSync } = figlet
 import axios from "axios";
 
-let url = 'https://pokeapi.co/api/v2/pokemon/'+Math.floor(Math.random()*1008 + 1)
-axios.get(url)
-  .then(function (response) {
-    let name = response.data.name
-    console.log(textSync(name, {
-        font: 'Cosmike',
-        horizontalLayout: 'default',
-        verticalLayout: 'default',
-        width: 100,
-        whitespaceBreak: true
-    }));
-  })
-  .catch(function (error) {
-    console.log(error);
+async function getPokemon() {
+  let url = 'https://pokeapi.co/api/v2/pokemon/' + Math.floor(Math.random()*1008 + 1)
+  const response = await axios.get(url)
+  let name = response.data.name
+  figlet.text(name, {
+    font: 'Cosmike',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 100,
+    whitespaceBreak: true
+  }, function(error, data) {
+    if (error) {
+        console.log('Text did not load');
+        console.dir(error);
+        return;
+    }
+    console.log(data);
   });
+}
+
+try {
+    await getPokemon()
+} catch (error) {
+    console.log("No Pokemon Found")
+    console.dir(error)
+}
